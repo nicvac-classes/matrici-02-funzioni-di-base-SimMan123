5462c848eae7a23e749ec8ecbe69440e89ba90c4
SimMan123 <simone.manzari2@marconibari.edu.it>
2025-11-10 15:14:37 +0000

    eserczio

--- Esercizio.java
+++ Esercizio.java
@@ -8,22 +8,38 @@ class Esercizio {
 
     public static Scanner in = new Scanner( System.in );
 
-    public static void assegnaMatrice( /*scrivere qui i parametri richiesti dall'esercizio*/ ) {
-        // SCRIVERE QUI IL CODICE RICHIESTO DALL'ESERCIZIO
-    }
-
-    public static void azzeraMatrice( /*scrivere qui i parametri richiesti dall'esercizio*/ ) {
-        //SCRIVERE QUI IL CODICE RICHIESTO DALL'ESERCIZIO
-    }
-
-    public static void riempiCasuale( /*scrivere qui i parametri richiesti dall'esercizio*/ ) {
-        Random rand = new Random();
-        //SCRIVERE QUI IL CODICE RICHIESTO DALL'ESERCIZIO
-    }
-
-    public static void main(String args[]) {
-        //SCRIVERE QUI IL CODICE RICHIESTO DALL'ESERCIZIO  
+    public static void assegnaMatrice( int[][] M, int RIGHE, int COLONNE, int valore ) {
+    for (int i=0; i <= RIGHE-1; i=i+1 ) {
+        for (int j=0; j <= COLONNE-1; j=j+1) {
+            M[i][j] = valore;
+        }
     }
 }
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+    public static void azzera( int[][] M, int RIGHE, int COLONNE, int valore ) {
+    for (int i=0; i <= RIGHE-1; i=i+1 ) {
+        for (int j=0; j <= COLONNE-1; j=j+1) {
+            M[i][j] = 0;
+        }
+    }
+}
+
+    public static void riempiCasuale( int[][] M, int RIGHE, int COLONNE, int valMin, int valMax) {
+    Random rand = new Random();
+    for (int i=0; i <= RIGHE-1; i=i+1 ) {
+        for (int j=0; j <= COLONNE-1; j=j+1) {
+            M[i][j] = valMin + rand.nextInt((valMax+1)-valMin);
+        }
+    }
+}
+
+    public static void main(String args[]) {
+int[][] matR = new int[RIGHE][COLONNE];
+  int valMin = 10;
+  int valMax = RIGHE*COLONNE*10;
+  riempiCasuale(matR, RIGHE, COLONNE, valMin, valMax);
+  System.out.println("Contenuto della matrice matR:");
+  UtilsMatrice.visualizza(matR);    
+    }
+}
+
